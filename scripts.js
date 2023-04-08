ebe3902e9a5d3a5880cccf124e360f518fc97207
theopiedade <theopiedade@hotmail.com>
2023-04-07 22:20:05 -0300

    Cards With Parrots Ok

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-alert("Olá");
+//alert("Olá");
 let cards = 1;
 while (cards%2 !== 0 || cards < 4 || cards > 14) { 
     cards = prompt("Com quantas cartas quer jogar? (Pares de 4 a 14)");
@@ -6,6 +6,6 @@ while (cards%2 !== 0 || cards < 4 || cards > 14) {
 let element = document.querySelector('.container_cards');
 element.innerHTML = '';
 for (let i=0;i<cards;i++) {
-    element.innerHTML += '<div class="cards"></div>';
+    element.innerHTML += '<div class="cards"><img class="back" src="img/back.png"/></div>';
 }
 
